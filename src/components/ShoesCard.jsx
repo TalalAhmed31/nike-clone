d0c756e29810c2ddeeb4c040b1905113ed2a7f43
Talal Ahmed <talalsidd31@gmail.com>
2024-04-01 04:17:58 +0500

    completed ui of shoes componenrs

--- src/components/ShoesCard.jsx
+++ src/components/ShoesCard.jsx
@@ -17,7 +17,17 @@ const ShoesCard = ({ imgURL, changeBigShoeImage, bigShoeImage }) => {
     `}
       onClick={handleClick}
     >
-      shoe card
+
+    <div className="flex justify-center items-center bg-card
+    bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+      <img 
+        src={imgURL.thumbnail}
+        alt="shoe Collection"
+        width={127}
+        height={103}
+        className="object-contain"
+      />
+      </div>
     </div>
   );
 };
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -76,7 +76,8 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
 
-        <div>
+        <div className="flex sm:ga-6 gap-4 absolute -bottom-[5%]
+        sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe, index) => (
             <div key={index}>
               <ShoesCard
