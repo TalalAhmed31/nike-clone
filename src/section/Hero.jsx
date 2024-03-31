73e45165272d8d7d5194d6e7da75cf0d186310db
Talal Ahmed <talalsidd31@gmail.com>
2024-03-31 16:41:46 +0500

    hero section

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,7 +1,13 @@
 const Hero = () => {
   return (
-    <section>
+    <section
+    id="home"
+    className="w-full flex xl:flex-row flex-col 
+    justify-center min-h-screen gap-10 max-container">
       
+      <div className="relative">
+
+      </div>
     </section>
   )
 }
