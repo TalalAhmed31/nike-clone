43ef519def2ed6469af96c7a91ff632f7dbba4fd
Talal Ahmed <talalsidd31@gmail.com>
2024-04-05 03:21:42 +0500

    testing

--- src/section/PopularProducts.jsx
+++ src/section/PopularProducts.jsx
@@ -15,6 +15,13 @@ const PopularProducts = () => {
           selections. Discover a world of comfort, design and value
         </p>
       </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1
+      sm:gap-4 gap-14 ">
+      {products.map((product, index) => (
+        console.log(product.name)
+      ))}
+      </div>
     </section>
   );
 };
