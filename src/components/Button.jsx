661ee81658c3e3a66530e656291a5fa704bf9c03
Talal Ahmed <talalsidd31@gmail.com>
2024-04-16 14:43:17 +0500

    special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,27 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor,
+textColor, borderColor}) => {
   return (
     <button
-    className="flex justify-center gap-2 px-7
+      className={`flex justify-center gap-2 px-7
     py-4 border font-montserrat text-lg leading-none
-    bg-coral-red text-white rounded-full border-coral-red hover:bg-white
-    hover:transition-all hover:text-coral-red">
-        {label}
-        {iconURL && <img
-        src={iconURL}
-        alt="arrow-righticon"
-        className="ml rounded-full w-5 h-5"
-         />}
-    </button>
-  )
-}
 
-export default Button
+    ${
+      backgroundColor ? 
+      `${backgroundColor} ${textColor} ${borderColor}`
+      : "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}
+    
+
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow-righticon"
+          className="ml rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
--- src/section/SpecialOffer.jsx
+++ src/section/SpecialOffer.jsx
@@ -1,5 +1,6 @@
 import { offer } from "../assets/images"
-
+import Button from "../components/Button"
+import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
@@ -11,6 +12,29 @@ const SpecialOffer = () => {
       className="object-contain w-full"
     /> 
     </div> 
+    <div className="flex flex-1 flex-col">
+        <h2
+          className="mt-10 font-palanquin text-4xl capitalize font-bold
+          lg:max-w-lg"
+        >
+          <span className="text-coral-red"> Special </span>
+          Offer
+        </h2>
+
+        <p
+          className="mt-4 lg:max-w-lg info-text"
+        >
+          Ensuring premium comfort and style, our meticuslously crafted footwear is designed
+          to elevate your experience.
+        </p>
+        
+        <div className="mt-11 flex flex-wrap gap-4">
+        <Button label="Shop Now" iconURL={arrowRight} />
+        <Button label="Learn More" backgroundColor="bg-white"
+        borderColor="border-slate-gray"
+        textColor="text-slate-gray" />
+        </div>
+      </div>
     </section>
   )
 }
